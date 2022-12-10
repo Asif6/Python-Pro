78a5ffa40cbb2d90efe0047a2ee5047e0ea6db60
= <=>
2022-12-11 04:02:47 +0600

    Input function in python

--- Project-1.py
+++ Project-1.py
@@ -3,3 +3,8 @@ print("The function is declared like this:")
 print("print( 'what to print' )")
 print("Day 1- Python Print Function\nThe functio is declared like this:\nprint( 'what to print' )")
 print("Asif"+" "+"Foysal")
+input("What is your name: ") # This line take input from user
+
+print("Hello " + input("What is your name?")) # This line take the input first then print the output 
+
+
