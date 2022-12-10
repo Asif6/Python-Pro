afdff7a9fd41b635afb8796c9a73e02d194a6d3e
= <=>
2022-12-11 03:10:12 +0600

    MAIN

--- Project-1.py
+++ Project-1.py
@@ -1,3 +1,5 @@
 print("Day 1- Python Print Function")
 print("The function is declared like this:")
 print("print( 'what to print' )")
+print("Day 1- Python Print Function\nThe functio is declared like this:\nprint( 'what to print' )")
+print("Day 1- Python Print Function\nThe functio is declared like this:\nprint( 'what to print' )")
